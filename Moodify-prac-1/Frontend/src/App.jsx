f3f4c679f91d3df033026c74a3e23804efe42fc8
Notanormaldev <harshpatel20050@gmail.com>
2026-04-24 14:14:25 +0530

    Face detection prac

---
+++ Moodify-prac-1/Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Expression from './Feature/Expression/components/Expression'
+
+function App() {
+  return (
+    <div>
+      <Expression/>
+    </div>
+  )
+}
+
+export default App
