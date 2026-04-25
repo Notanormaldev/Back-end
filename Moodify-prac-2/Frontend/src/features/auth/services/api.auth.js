feeee60426e1de7600de225964d834caf20984f1
Notanormaldev <harshpatel20050@gmail.com>
2026-04-25 15:11:32 +0530

    Api layer

---
+++ Moodify-prac-2/Frontend/src/features/auth/services/api.auth.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const api=axios.create({
+    baseURL:"http://localhost:3000",
+    withCredentials:true
+})
+
+
+export  async function register({email,username,password}){
+    const res = await api.post('/register',{
+        username,email,password
+    })
+    return res.data
+}
+
+
+export async function login({email,username,password}){
+    const res = await api.post('/login',{
+        username,email,password
+    })
+    console.log(res.data);
+    return res.data
+    
+}
+
+export async function getme(){
+    const res = await api.get('/get-me')
+    return res.data
+}
+export async function logout(){
+    const res= await api.get('/logout')
+    return res.data
+}
