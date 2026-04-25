77cad82ac0f78cc8218a15593e3177c9893b18c7
Notanormaldev <harshpatel20050@gmail.com>
2026-04-25 10:12:34 +0530

    Expression detection

---
+++ Moodify-prac-2/Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FaceExpression from './features/Expression/components/Faceexpression'
+
+function App() {
+  return (
+    <div>
+      <FaceExpression/>
+    </div>
+  )
+}
+
+export default App
