6e5078f5ab5fd5aeb21e5a340e4fbe88cf83a868
Notanormaldev <harshpatel20050@gmail.com>
2026-05-01 14:47:50 +0530

    Add ChatGoogleGenerativeAI integration and update dependencies

--- GEN-AI/index.js
+++ GEN-AI/index.js
@@ -1,10 +1,26 @@
 import readline from 'readline';
 
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash-lite",
+  apiKey: "your-api-key"
+});
+
+
+
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+
+
+``
+
+
+
 
 rl.question("what is your name:",(name)=>{
     console.log(`hello ${name}!`);
