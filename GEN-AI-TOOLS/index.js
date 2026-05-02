f8f203b97e7fa7aeb5ac13350b938b8260e5876a
Notanormaldev <harshpatel20050@gmail.com>
2026-05-02 14:20:02 +0530

    Add nodemailer integration for email functionality and update dependencies

--- GEN-AI-TOOLS/index.js
+++ GEN-AI-TOOLS/index.js
@@ -2,6 +2,49 @@ import 'dotenv/config'
 import readline from 'readline/promises'
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { HumanMessage } from 'langchain';
+import nodemailer from 'nodemailer';
+import * as z from "zod"
+import { tool } from "langchain"
+
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.EMAIL_USER,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+  },
+});
+
+
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('Error connecting to email server:', error);
+  } else {
+    console.log('Email server is ready to send messages');
+  }
+});
+
+async function sendEmail({to,html,subject,text=''}){
+   const emailoption={
+      from:process.env.USER,
+      to,
+      subject,
+      html,
+      text
+   }
+
+   const dts = await model.sendmail(emailoption)
+   console.log("Send email",dts);
+
+   return ("email sent sucesfully to:"+ to)
+   
+
+}
+
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
