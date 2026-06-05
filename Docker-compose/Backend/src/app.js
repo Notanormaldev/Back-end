c69dad993d6618d1682e9df3ab2dfb1c602a59e1
Notanormaldev <harshpatel20050@gmail.com>
2026-06-05 17:41:52 +0530

    feat: add Docker configuration with Dockerfile and docker-compose.yml

--- Docker-compose/Backend/src/app.js
+++ Docker-compose/Backend/src/app.js
@@ -4,12 +4,12 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World');
 });
 
 app.get('/api/data', (req, res) => {
   const data = {
-    message: 'This is some sample data from the backend.'};
+    message: 'This is some  data from the backend.'};
   res.json(data);
 });
 
