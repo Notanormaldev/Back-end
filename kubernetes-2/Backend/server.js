7c8598dc16ce709d7d15e1cccfde23707b071e1e
Notanormaldev <harshpatel20050@gmail.com>
2026-06-12 17:16:07 +0530

    feat: add initial server setup with Express and create Kubernetes configuration files

---
+++ kubernetes-2/Backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+
+
+app.get('/', (req, res) => {
+    let sum = 0;
+    for (let i = 0; i < 1000000000; i++) {
+        sum += i;
+    }
+    res.json({ sum });
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+    
+     
