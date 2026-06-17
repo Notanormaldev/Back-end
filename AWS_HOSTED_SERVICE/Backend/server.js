e59e0f30d6a78cddf63428ee4d5f802b73e48d62
Notanormaldev <harshpatel20050@gmail.com>
2026-06-17 10:06:36 +0530

    feat: initialize frontend with React and Vite setup
    - Added main application file (App.jsx) with basic structure and state management.
    - Created CSS file (App.css) for styling components.
    - Included assets for logos and hero image.
    - Configured Vite for development with React plugin.
    - Established global styles in index.css for consistent theming.
    - Implemented responsive design for better usability across devices.

--- AWS_HOSTED_SERVICE/Backend/server.js
+++ AWS_HOSTED_SERVICE/Backend/server.js
@@ -1,9 +1,26 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+// import path from 'path';
+// import { fileURLToPath } from 'url'
 
+// const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(morgan('dev'))
+
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.get('*name', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
