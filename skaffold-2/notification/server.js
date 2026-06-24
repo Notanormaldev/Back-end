9c012dfdf30ebd89b811c0aae1a840a9b01e936a
Notanormaldev <harshpatel20050@gmail.com>
2026-06-24 11:22:24 +0530

    feat: initialize core and notification microservices with Docker and Skaffold configuration

---
+++ skaffold-2/notification/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import morgan from "morgan";
+
+
+const app = express();
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  res.send("Hello, World! This is the Notification Service.");
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}   );
