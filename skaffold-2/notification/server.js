4cacead9ca1060f86e0536531aecdcc15605615e
Notanormaldev <harshpatel20050@gmail.com>
2026-06-24 13:48:27 +0530

    feat: initialize core and notification service packages with express server boilerplate

--- skaffold-2/notification/server.js
+++ skaffold-2/notification/server.js
@@ -5,7 +5,7 @@ import morgan from "morgan";
 const app = express();
 app.use(morgan("dev"));
 app.get("/", (req, res) => {
-  res.send("Hello, World! This is the Notification Service.");
+  res.send("Hello, World! This is the Notification Service......");
 });
 
 const PORT = process.env.PORT || 8080;
