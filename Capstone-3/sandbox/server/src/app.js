e5e578cebd8f9fbca159c7a48ac3531749f80f3b
Notanormaldev <harshpatel20050@gmail.com>
2026-07-04 18:32:25 +0530

    feat: initialize express server with health check endpoint and core middleware

---
+++ Capstone-3/sandbox/server/src/app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import morgan from 'morgan'
+
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(morgan('dev'))
+
+
+app.use("/api/sandox/health",(req,res)=>{
+    return res.status(200).json({
+        message:"Sandbox is healthy",
+        success:true,
+        
+    })
+})
+
+export default app
