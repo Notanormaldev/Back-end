b294d02fd6286d14016c8ef74e930354d6cdf731
Notanormaldev <harshpatel20050@gmail.com>
2026-07-05 10:13:26 +0530

    feat: implement sandbox creation service and traffic router with Kubernetes deployment configurations

--- Capstone-3/sandbox/router/src/app.js
+++ Capstone-3/sandbox/router/src/app.js
@@ -11,6 +11,34 @@ app.get("/api/router/health",(req,res)=>{
     res.send("Router API is running")
 })
 
+const proxies={}
+function getproxy(sandboxid){
+    const target=`http://sandbox-service-${sandboxid}`
+
+    if(!proxies[sandboxid]){
+        proxies[sandboxid]=createProxyMiddleware({
+            target,
+            changeOrigin:true,
+            ws:true
+        })
+    }
+    return proxies[sandboxid]
+}
+
+app.use((req, res, next) => {
+    const host = req.headers.host
+
+    const sandboxid = host.split('.')[0]
+
+
+    if(host.split('.')[1]=="agent"){
+        // return getagentproxy(sandboxid)(req,res,next);
+    }else{
+
+        return getproxy(sandboxid)(req,res,next);
+    }
+
+})
 
 
 
--- Capstone-3/sandbox/server/src/app.js
+++ Capstone-3/sandbox/server/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import morgan from 'morgan'
-
+import { v4 as uuid } from "uuid"
+import { createpod } from '../kubernetes/pod.js';
+import { createservice } from '../kubernetes/service.js';
 
 const app = express();
 app.use(express.json())
@@ -9,12 +11,28 @@ app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
 
-app.use("/api/sandbox/health",(req,res)=>{
+app.get("/api/sandbox/health",(req,res)=>{
     return res.status(200).json({
         message:"Sandbox is healthy",
         success:true,
         
     })
+})
+
+app.post('/api/sandbox/start',async (req,res)=>{
+    const sandboxid = uuid() 
+
+    await Promise.all([
+        createpod(sandboxid),
+        createservice(sandboxid)
+    ])
+
+   return res.status(201).json({
+    message:"Sandbox created successfully",
+    sandboxid:sandboxid,
+    preview:`http://${sandboxid}.preview.localhost`
+
+  })
 })
 
 export default app
