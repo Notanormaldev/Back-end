544fa468cfcf779da74729f4a288c6fc76933e97
Notanormaldev <harshpatel20050@gmail.com>
2026-07-09 10:29:44 +0530

    feat: initialize Vite configuration with React plugin and development server settings

--- Capstone-4/sandbox/template/vite.config.js
+++ Capstone-4/sandbox/template/vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: true
+  },
+  watch:{
+    usePolling: true,
+    interval:300,
+    ignored:["node_modules"] 
+  }
 })
