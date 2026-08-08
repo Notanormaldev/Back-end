fb248b2937745e022ce2bc34dd29517d81d2eb90
Notanormaldev <harshpatel20050@gmail.com>
2026-08-08 10:22:34 +0530

    EJS

--- Redis/server.js
+++ Redis/server.js
@@ -39,16 +39,19 @@ redis.once("error", (err) => {
 const app = express();
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 const globalLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,  // 1 minutes
-  max: 100,                    // 100 requests per window per IP
-  message: {
-    error: 'Too many requests. Please try again later.'
-  },
-  statusCode: 429,
-  standardHeaders: true,   // sends RateLimit-* headers
-//   legacyHeaders: false,
+    windowMs: 1 * 60 * 1000,  // 1 minutes
+    max: 100,                    // 100 requests per window per IP
+    message: {
+        error: 'Too many requests. Please try again later.'
+    },
+    statusCode: 429,
+    standardHeaders: true,   // sends RateLimit-* headers
+    //   legacyHeaders: false,
 });
 
 
@@ -61,12 +64,24 @@ app.use(globalLimiter);
 
 
 
-app.get('/',(req,res)=>{
-    let sum=0;
-    for(let i=0;i<=100000000;i++){
-        sum+=i;
+app.get('/', (req, res) => {
+    let sum = 0;
+    for (let i = 0; i <= 100000000; i++) {
+        sum += i;
     }
-    res.json({result:+sum})
+    res.json({ result: +sum })
+})
+
+
+app.get('/ejs', (req, res) => {
+    res.render('index', {
+        username: "doremon",
+        bio: "I am a cat 🐱",
+        image: "https://images.unsplash.com/photo-1518893647222-31f6b0dc2fb6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmxhY2slMjBuJTIwd2hpdGV8ZW58MHx8MHx8fDA%3D"
+    }
+
+    
+    )
 })
 app.get("/user/:id", async (req, res) => {
     try {
@@ -93,7 +108,7 @@ app.get("/user/:id", async (req, res) => {
 
 app.post("/createuser", async (req, res) => {
     try {
-      
+
 
         const user = await Usermodel.create(req.body);
 
