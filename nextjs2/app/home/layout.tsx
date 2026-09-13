f2fd8c35e9c88c53c9d85ebed578a63eb5947d4b
Notanormaldev <harshpatel20050@gmail.com>
2026-09-13 15:56:16 +0530

    feat: add home page and layout components

---
+++ nextjs2/app/home/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function layout({children}) {
+  
+  return (
+    <html
+      lang="en"
+      className={` h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
+
+
+
+export default layout
