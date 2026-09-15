46882a9838f22a91d73e082bd23fbf237bf586ad
Notanormaldev <harshpatel20050@gmail.com>
2026-09-15 10:30:20 +0530

    feat: implement product page with API data fetching and product grid

--- ecom-nxt/src/app/(main)/product/page.js
+++ ecom-nxt/src/app/(main)/product/page.js
@@ -1,23 +1,20 @@
-
-import Protected from '../../components/Protected.js';
-import ProductCard from '../../components/ProductCard.js';
-import React from 'react'
+import Protected from "@/components/Protected";
+import ProductCard from "@/components/ProductCard";
+import React from "react";
 
 async function page() {
-    const res = await fetch('https://fakestoreapi.com/products')
-    const products = await res.json()
-    console.log(products);
-    
+  const res = await fetch("https://fakestoreapi.com/products");
+  const products = await res.json();
 
   return (
     <Protected>
-    <div className=' grid grid-cols-4 gap-5 p-10 '>
-
-        {products.map(product => (
-            <ProductCard key={product.id} product={product} />
+      <div className="grid grid-cols-4 gap-5 p-10">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
         ))}
-    </div></Protected>
-  )
+      </div>
+    </Protected>
+  );
 }
 
-export default page
+export default page;
